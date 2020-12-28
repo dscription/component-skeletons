cfe00ecca062e9bdfe14a9451d245120208279db
dscription <dscriptionplayground@gmail.com>
2020-12-27 19:17:38 -0500

    make api call to jsonplaceholder

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const User = () => {
+  const [profile, setProfile] = useState(null);
+  useEffect(() => {
+    setTimeout(async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users/1')
+      const data = await res.json()
+      setProfile(data)
+    }, 5000);
+  });
   return (
     <div className="user">
       <h2>User Details</h2>
     </div>
-  )
-}
+  );
+};
 
-export default User
+export default User;
